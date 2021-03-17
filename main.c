09f7544e9e086cc66e23580792d4b92a4738ccb8
dgrubb <davidanthonygrubb@gmail.com>
2021-03-17 16:51:32 -0500

    Tweak skeleton to new project.

--- main.c
+++ main.c
@@ -28,19 +28,9 @@
 
 #include <jo/jo.h>
 
-void			my_draw(void)
-{
-	jo_sprite_draw3D(0, 0, 0, 500);
-}
-
-void			jo_main(void)
+void
+jo_main(void)
 {
 	jo_core_init(JO_COLOR_Black);
-	jo_sprite_add_tga("TEX", "SONIC.TGA", JO_COLOR_Transparent);
-	jo_core_add_callback(my_draw);
 	jo_core_run();
 }
-
-/*
-** END OF FILE
-*/
