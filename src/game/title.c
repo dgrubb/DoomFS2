fc7646c53d6271a44ee6b19fbefb34699f1e995c
dgrubb <davidanthonygrubb@gmail.com>
2021-04-05 20:03:49 -0500

    Finish up software rendered fire effect.

--- src/game/title.c
+++ src/game/title.c
@@ -10,45 +10,42 @@
 #include "game/title.h"
 #include "video/video.h"
 
-bool notyet = true;
-
 const jo_color fire_palette_test[] = {
     JO_COLOR_Transparent, /* Transparent */
-    JO_COLOR_RGB(31, 7, 7), /* 31, 7, 7 */
-    JO_COLOR_RGB(47, 15, 7), /* 47, 15, 7 */
-    JO_COLOR_RGB(71, 15, 7), /* 71, 15, 7 */
-    JO_COLOR_RGB(87, 23, 7), /* 87, 23, 7 */
-    JO_COLOR_RGB(103, 31, 7), /* 103, 31, 7 */
-    JO_COLOR_RGB(119, 31, 7), /* 119, 31, 7 */
-    JO_COLOR_RGB(143, 39, 7), /* 143, 39, 7 */
-    JO_COLOR_RGB(159, 47, 7), /* 159, 47, 7 */
-    JO_COLOR_RGB(175, 63, 7), /* 175, 63, 7 */
-    JO_COLOR_RGB(191, 71, 7), /* 191, 71, 7 */
-    JO_COLOR_RGB(199, 71, 7), /* 199, 71, 7 */
-    JO_COLOR_RGB(223, 79, 7), /* 223, 79, 7 */
-    JO_COLOR_RGB(223, 87, 7), /* 223, 87, 7 */
-    JO_COLOR_RGB(223, 87, 7), /* 223, 87, 7 */
-    JO_COLOR_RGB(215, 95, 7), /* 215, 95, 7 */
-    JO_COLOR_RGB(215, 103, 15), /* 215, 103, 15 */
-    JO_COLOR_RGB(207, 111, 15), /* 207, 111, 15 */
-    JO_COLOR_RGB(207, 119, 15), /* 207, 119, 15 */
-    JO_COLOR_RGB(207, 127, 15), /* 207, 127, 15 */
-    JO_COLOR_RGB(207, 135, 23), /* 207, 135, 23 */
-    JO_COLOR_RGB(199, 135, 23), /* 199, 135, 23 */
-    JO_COLOR_RGB(199, 135, 23), /* 199, 135, 23 */
-    JO_COLOR_RGB(199, 135, 23), /* 199, 135, 23 */
-    JO_COLOR_RGB(199, 151, 31), /* 199, 151, 31 */
-    JO_COLOR_RGB(191, 159, 31), /* 191, 159, 31 */
-    JO_COLOR_RGB(191, 159, 31), /* 191, 159, 31 */
-    JO_COLOR_RGB(191, 167, 39), /* 191, 167, 39 */
-    JO_COLOR_RGB(191, 167, 39), /* 191, 167, 39 */
-    JO_COLOR_RGB(191, 175, 47), /* 191, 175, 47 */
-    JO_COLOR_RGB(183, 175, 47), /* 183, 175, 47 */
-    JO_COLOR_RGB(183, 183, 47), /* 183, 183, 47 */
-    JO_COLOR_RGB(183, 183, 55), /* 183, 183, 55 */
-    JO_COLOR_RGB(207, 207, 111), /* 207, 207, 111 */
-    JO_COLOR_RGB(223, 223, 159), /* 223, 223, 159 */
-    JO_COLOR_RGB(239, 239, 199), /* 239, 239, 199 */
+    32771,
+    32085,
+    32808,
+    32842,
+    32876,
+    32878,
+    32913,
+    32947,
+    33013,
+    33047,
+    33048,
+    33083,
+    33115,
+    33115,
+    33146,
+    34202,
+    34233,
+    34265,
+    34297,
+    35353,
+    35352,
+    35352,
+    36440,
+    36471,
+    36471,
+    37527,
+    37527,
+    38583,
+    38582,
+    38614,
+    39638,
+    46905,
+    53115,
+    58301,
     JO_COLOR_White
 };
 
@@ -56,7 +53,7 @@ int
 get_palette_idx(jo_color color)
 {
     int i=0;
-    for (i=0; i<37; i++) {
+    for (i=0; i<36; i++) {
         if (color == fire_palette_test[i]) {
             return i;
         }
@@ -91,8 +88,6 @@ title_start()
     for (int i=0; i<fire_width; i++) {
         fire->color_buffer[i + (fire_height-1) * fire->vram_size.width] = JO_COLOR_White;
     }
-//    jo_core_add_slave_callback(do_fire);
-    notyet = false;
 }
 
 /* Deallocate VDP1 memory allocated when starting the
@@ -107,7 +102,6 @@ title_stop(game_action_t exit_state)
 void
 do_fire()
 {
-    if (notyet) return;
     for (int x=0; x<fire_width; x++) {
         for (int y=1; y<fire_height; y++) {
             spread_fire(y * fire_width + x);
@@ -120,9 +114,11 @@ void
 spread_fire(int from)
 {
     int rand = jo_random(3) & 3;
+    fire->color_buffer[from - fire_width - rand + 1] = fire_palette_test[get_palette_idx(fire->color_buffer[from]) - (rand & 1)];
+    /*
     int to = from - fire_width - rand + 1;
-    int idx = get_palette_idx(fire->color_buffer[from]) - (rand & 1);
-    fire->color_buffer[to] = fire_palette_test[idx];
+    fire->color_buffer[to] = fire->color_buffer[from] - (rand & 1);
+    */
 }
 
 /* Update fire animation. Call repeatedly between screen
